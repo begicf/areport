92bda6be10c3dedccbc6dcdcd0e440ec7161c943
Fuad Begic <fuad.begic@gmail.com>
2020-02-02 18:10:40 +0100

    Upload fix

--- app/Http/Controllers/Upload/UploadController.php
+++ app/Http/Controllers/Upload/UploadController.php
@@ -6,7 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Model\UploadTaxonomy;
 use Illuminate\Http\Request;
 
-
+/**
+ * Class UploadController
+ * @category
+ * @package App\Http\Controllers\Upload
+ * @author Fuad Begic <fuad.begic@gmail.com>
+ * Date: 02/02/2020
+ * Time: 18:08
+ */
 class UploadController extends Controller
 {
     public function __construct()
@@ -14,36 +21,47 @@ class UploadController extends Controller
         $this->middleware('auth');
     }
 
+    /**
+     * @return View
+     */
     public function index()
     {
-
 
         return view('upload/upload');
     }
 
+    /**
+     * @param Request $request
+     * @return Http\Response
+     */
     public function store(Request $request)
     {
+        $path = 'file';
+
         $_name = $request->file('file')->getClientOriginalName();
-        $file = $request->file('file')->store('file', 'public');
+        $file = $request->file('file')->store($path, 'public');
 
         $zip = new \ZipArchive();
 
         $res = $zip->open(storage_path('app/public/' . $file));
 
-        $name = substr($_name, 0, strpos($_name, "."));
+        $name = $path . DIRECTORY_SEPARATOR . substr($_name, 0, strpos($_name, "."));
 
         if ($res === TRUE):
 
-            $zip->extractTo(storage_path('app/public/file/' . $name));
+            $zip->extractTo(storage_path('app/public/' . $name));
 
             $zip->close();
+            // remove zip file
+            //  unlink(storage_path('app/public/' . $file));
+
+            UploadTaxonomy::create([
+                'file' => $file,
+                'original_name' => $name
+            ]);
 
         endif;
 
-        UploadTaxonomy::create([
-            'file' => $file,
-            'original_name' => $name
-        ]);
 
         if ($request->wantsJson()) {
             return response([], 204);
