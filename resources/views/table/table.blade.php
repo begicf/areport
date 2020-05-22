a62d72c5d32774c4e31ea93e4280102ed56c21fc
begicf <fuad.begic@fba.ba>
2020-05-22 21:15:06 +0200

    beta_1.0

--- resources/views/table/table.blade.php
+++ resources/views/table/table.blade.php
@@ -4,27 +4,64 @@
 
     <div class="container-fluid">
 
-        <div class="row pb-3">
-            <div class="col col-lg-5">
+        <div class="row">
 
-                <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-                    <div class="btn-group mr-2" role="group" aria-label="First group">
-                        <button type="button" class="btn btn-primary" data-toggle="modal" onclick="module()"><i
-                                class="fas fa-table"></i></button>
-                    </div>
-                    <div class="btn-group mr-4" role="group" aria-label="First group">
-                        <select id="group" onchange="changeTable(this,'G')" class="form-control">
-                            @foreach($groups as $key=>$row)
-                                <option value="{{$row}}">
-                                    {{$key}}
-                                </option>
-                            @endforeach
-                        </select>
+            <div class="col-auto mr-auto">
+                <div class="form-row align-items-center">
+
+                    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                        <div class="btn-group mr-2" role="group" aria-label="First group">
+                            <button type="button" class="btn btn-primary" data-toggle="modal" onclick="module()"><i
+                                    class="fas fa-table"></i></button>
+                        </div>
+                        <div class="btn-group mr-4" role="group" aria-label="Second group">
+                            <select id="group" onchange="changeTable(this,'G')" class="form-control">
+                                @foreach($groups as $key=>$row)
+                                    <option value="{{$row}}">
+                                        {{$key}}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+                            <div class="btn-group mr-4" role="group" aria-label="Third group">
+
+                                <div class="btn-group" role="group">
+                                    <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Export
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                                        <a class="dropdown-item" href="#"><i class="text-success fas fa-file-excel"></i>
+                                            Export to .xlsx</a>
+                                        <a class="dropdown-item" href="#"><i class="text-danger fas fa-file-pdf"></i>
+                                            Export to .pdf</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
             </div>
+
+            <div class="col-auto">
+
+                <form class="form-inline float-right">
+                    <div class="form-group">
+                        <label for="exampleFormControlFile1">*.xlsx </label>
+                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Import</button>
+
+                </form>
+
+            </div>
+
         </div>
+
 
         <!--Button Table Group -->
         <div id="button_group" class="col-lg-12 p-2"></div>
@@ -32,6 +69,13 @@
         <div id="sheets" class="col-lg-3 p-2"></div>
         <!--Table -->
         <div id="tab" class="overflow-auto"></div>
+
+
+        <div id="openY" class="form-group" style="display: none">
+            <a class="btn btn-light" id="addRow">+</a>
+            <a class="btn btn-light" id="delRow">-</a>
+        </div>
+
 
     </div>
 
@@ -87,6 +131,8 @@
         var group;
         var table = null;
 
+        $("#openY").hide();
+
         if (type == 'G') {
             group = selectedOb.value;
         } else {
@@ -116,7 +162,57 @@
             $('#button_group').html(response.data.groups);
 
             $('#sheet').selectpicker();
+
+
+            if (response.data.aspectNode == true) {
+                var rowCount = null;
+                rowCount = $('#table tbody').find('tr').length;
+                console.log(rowCount);
+                $("#openY").show();
+                /*add rows*/
+                $("#addRow").on('click', function (event) {
+                    event.preventDefault();
+
+                    $('#table tbody>tr:last').clone(true).each(function () {
+
+                        $(this).find('td input, td select').each(function () {
+
+
+                            var name = $(this).attr('name').replace(/(c\d*r)\d*([^]*)/, "$1" + rowCou
+
+
+                            nt + "$2");
+                            var id = name.substring(0, name.indexOf('['));
+                            $(this).attr('name', name);
+                            if ($(this).attr('type') != 'hidden') {
+                                $(this).val('');
+                                $(this).attr('id', id);
+                            }
+                        });
+
+                    }).insertAfter('#table tbody>tr:last');
+                    /* $('#table tbody>tr:last').each(function () {
+                     this.reset();
+                     });*/
+                    //dataSet();
+                    rowCount++;
+                    return false;
+                });
+
+                /* delete rows */
+                var $tbody = $("#table tbody");
+                $("#delRow").click(function () {
+                    var $last = $tbody.find('tr:last');
+                    if ($last.is(':first-child')) {
+                        alert('You cannot delete the last one!');
+                    } else {
+                        $last.remove();
+                        rowCount--;
+                    }
+                });
+            }
         })
     }
+
 
 </script>
