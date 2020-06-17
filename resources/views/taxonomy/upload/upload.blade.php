74c6f7ef61787090f335f862e559c791fd246858
begicf <fuad.begic@fba.ba>
2020-06-17 18:55:11 +0200

    Bug Fix

--- resources/views/taxonomy/upload/upload.blade.php
+++ resources/views/taxonomy/upload/upload.blade.php
@@ -9,7 +9,7 @@
             <div class="card-body">
 
                 <form class="form" action="/taxonomy/upload" method="post" enctype="multipart/form-data">
-                    {{ csrf_field() }}
+{{--                    {{ csrf_field() }}--}}
                     <div class="form-group">
                         <label for="tax">Taxonomy name</label>
                         <input type="text" name="name" class="form-control" id="tax" aria-describedby="TaxonomyName" placeholder="Taxonomy name" required>
@@ -20,7 +20,7 @@
                         <input id="zip" form-control-file type="file" accept="application/zip" required name="file">
                         <small class="form-text text-muted"><span class="text-danger">*</span> Only *.zip</small>
                     </div>
-                    <button class="btn btn-primary float-right" type="submit">Upload</button>
+                    <button class="btn btn-primary float-right" disabled type="submit">Upload</button>
                 </form>
 
             </div>
