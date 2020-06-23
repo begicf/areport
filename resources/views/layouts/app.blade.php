4866e36e76b9366aae05a902ce189a8b13e0f191
Fuad Begic <fuad.begic@gmail.com>
2020-06-23 08:22:41 +0200

    BugFix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Areport') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/jquerylib.js') }}" ></script>
